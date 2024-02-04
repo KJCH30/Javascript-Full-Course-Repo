5de005e268661076c8617f54d5fa1cd3c418f3bb
KJCH30 <89238948+KJCH30@users.noreply.github.com>
2024-02-04 05:35:12 +0000

    Conversion operation in JS

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "33"
+
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+//console.log(typeof (valueInNumber))
+//console.log(valueInNumber)
+
+/* Outputs if score is =
+"33" => 33 
+"33abc" => NaN (Not a Number)
+true => 1; false => 0 
+*/
+
+let isLoggedIn = "0"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+/* Output for the following inputs */
+// 1 => true; 0 => false
+//"" => false
+//"abc" => true
+
+let someNumber = 77
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber));
