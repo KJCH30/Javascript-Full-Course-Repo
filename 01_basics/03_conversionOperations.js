9948065aec0b37d59f499b6fea6d1512e770ead5
KJCH30 <89238948+KJCH30@users.noreply.github.com>
2024-02-04 06:48:19 +0000

    Some more conversions of datatypes in JS

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -23,5 +23,52 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 77
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof(stringNumber));
+// console.log(stringNumber)
+// console.log(typeof(stringNumber));
+
+/*************************OPERATIONS**************************************/
+
+let value = 3
+let negativeValue = -value
+// console.log(negativeValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "2") //by default it concatenates
+// console.log("1" + 2) //same as above
+// console.log("1" + 2 + 2); //here since the first value was in string, JS assumes that next values will be in string and concatenates them without any further calculations
+// console.log(1 + 2 + "2") // but in here, since the first two values are in integer/number, it first adds them and then concatenates the result with the string. This is due to the primitive value conversion rules of JS
+
+// console.log(+true) //it converts true to 1 because + is used
+// console.log(+"");  // it converts " " to 0
+// console.log(true + 2); //since value of true is 1 and we are adding 2, it adds the numbers and gives 3
+//All these conversions are not recommended in practice
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+//console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3" because it increments x but when returned, it returns the original value first
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4" because it increments a as well as returns the same whenever it is called
