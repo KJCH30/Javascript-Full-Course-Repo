4dbcb5dc017afe9a0c9a6ec9b546a16a3d9a7e9b
KJCH30 <89238948+KJCH30@users.noreply.github.com>
2024-02-17 04:49:51 +0000

    learnt how to pass objects  and arrays in a function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -37,3 +37,33 @@ function loginUserMessage(username = "Jish"){ // this pass Jish as by default na
 
 console.log(loginUserMessage("Kinjal"));
 console.log(loginUserMessage());
+
+function calculateCartPrice(...num1){ // ... is a rest operator which, combines all the passed values in a array
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 300, 400, 600));
+
+const product = {
+    productName : "Umbrella",
+    price: 199
+}
+
+function handleObject(anyObject){
+    // console.log(`Product name is ${anyObject.productName} and its price is ${anyObject.price}`);
+} 
+
+// handleObject(product)
+handleObject({
+    productName : "Boots",
+    price : 1999
+})
+
+const myArray = [100, 400, 500, 300]
+
+function returnSecondElement(getArray){
+    return getArray[1]
+}
+
+// console.log(`Second Element of the array ${myArray} is ${returnSecondElement(myArray)}`)
+console.log(returnSecondElement([400, 500, 100, 700]));
