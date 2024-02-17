ce960c7272867984e07dba6887f42dfee1f82310
KJCH30 <89238948+KJCH30@users.noreply.github.com>
2024-02-17 05:25:46 +0000

    nested scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,12 +6,30 @@ var c = 40
 if(true){
     //this is local scope
     let a = 50  // this follows the local scope constraints and cannot  be accessed outside this scope
-    console.log("Inside the scope a: ", a);
+    // console.log("Inside the scope a: ", a);
     const b = 90 // this follows the local scope constraints and cannot  be accessed outside this scope
-    console.log("Inside the scope b: ", b);
+    // console.log("Inside the scope b: ", b);
     var c = 400  // this doesn't allow the scope parameters and overrides any variable with the same name outside this scope
 }
 
-console.log("Outside scope a: ", a); //60
-console.log("Outside scope b: ", b); //70
-console.log(c); //400
+// console.log("Outside scope a: ", a); //60
+// console.log("Outside scope b: ", b); //70
+// console.log(c); //400
+
+/******************************************************************************************************** */
+
+// NESTED SCOPE: 
+
+function one (){
+    const username  = "Jishnu"
+
+    function two(){
+        const website = "GitHub"
+        console.log(username);
+    }
+   // console.log(website); //this line gives an error since website is defined inside the scope of two
+
+    two()
+}
+
+one()
